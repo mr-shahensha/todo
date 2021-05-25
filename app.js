768a02f47162a26c1751826701c4a541313a5deb
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2021-05-25 17:54:49 +0530

    okk

--- app.js
+++ app.js
@@ -39,5 +39,10 @@ function deletetodo(e){
         const todo=item.parentElement;
         todo.remove();
     }
+    
+    if (item.classList[0]=="complete-btn"){
+        const todo=item.parentElement;
+        todo.classList.toggle("completed");
+    }
 
 }
